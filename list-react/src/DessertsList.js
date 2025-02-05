25af5d15d290b4358f3bd684866e37a7424943c6
Renan Rosales <alfredorg9306@gmail.com>
2025-02-05 13:47:37 -0600

    Agregando las calorias y convertiendo a una lista ordenada

--- list-react/src/DessertsList.js
+++ list-react/src/DessertsList.js
@@ -1,15 +1,17 @@
 function DessertsList(props) {
-    const topDessert = props.desserts
-    .filter(dessert => dessert.calories < 500)
-    .sort((a,b) => b.calories - a.calories)
-    .map(topdesserts => (
-      <li>{topdesserts.name}</li>
+
+    const topDessert = props.desserts // ASIGNANDO EL VALOR DEL PROP EN EL APP LLAMADO desserts
+    .filter(dessert => dessert.calories < 500) // DADO QUE SE HACE UN FILTRO EN LAS INDICACIONES, SIEMPRE LOS FILTROS SE REALIZAR ANTES DE LA FUNCION MAP
+    .sort((a,b) => b.calories - a.calories) // ORDENANDO DE MAYOR A MENOR USANDO LA FUNCION SORT
+    .map(topdesserts => ( // FINALMENTE SE REALIZA LA EXTRACCION DE LA INFORMACION Y EN ESTE CASO REALIZAMOS EN UNA LISTA
+      <li>{`${topdesserts.name} - ${topdesserts.calories}`}</li>
     ));
+
   return(
 
-      <ul>
+      <ol>
           {topDessert}
-      </ul>
+      </ol>
   
   );
 }
